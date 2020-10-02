ff1a4d00e63564f5c2d2d6f6af92084c0932d6c9
k-watanabe14 <east-andante@hotmail.co.jp>
2020-10-02 11:25:03 +0900

    Initial Commit

---
+++ BarcodeScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by Kazuma Watanabe on 2020/10/02.
+//  Copyright © 2020 Kazuma Watanabe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
